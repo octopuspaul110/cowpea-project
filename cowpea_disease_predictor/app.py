d6b3cd975ec6735bbd0ec4c4449f372d29a6882e
Adegbite Paul <105382756+octopuspaul110@users.noreply.github.com>
2025-01-01 00:30:55 +0100

    Update app.py

--- cowpea_disease_predictor/app.py
+++ cowpea_disease_predictor/app.py
@@ -28,7 +28,7 @@ def initialize_db():
             NLB4 INTEGER,
             NLB6 INTEGER,
             NLB8 INTEGER,
-            Sample STRING,
+            Sample STRING
         );
         """))
 # Save data to database
